f64029de17720139ba2dbebddac25e476c2877fc
hjx13787 <2954775548@qq.com>
2015-11-18 15:04:16 +0800

    对普通用户充值时的限制：必须选择月租；必须写收费金额；必须写过期时间且比现在晚

--- src/main/java/com/donglu/carpark/ui/wizard/monthcharge/MonthlyUserPayWizard.java
+++ src/main/java/com/donglu/carpark/ui/wizard/monthcharge/MonthlyUserPayWizard.java
@@ -2,12 +2,17 @@ package com.donglu.carpark.ui.wizard.monthcharge;
 
 import com.dongluhitec.card.common.ui.AbstractWizard;
 import com.dongluhitec.card.common.ui.uitl.JFaceUtil;
+import com.dongluhitec.card.domain.db.singlecarpark.SingleCarparkMonthlyCharge;
+import com.dongluhitec.card.domain.util.StrUtil;
+
+import java.util.Date;
 
 import org.eclipse.jface.wizard.Wizard;
 
 public class MonthlyUserPayWizard extends Wizard implements AbstractWizard{
 	
 	private MonthlyUserPayModel model;
+	private MonthlyUserPayBasicPage page;
 	
 	
 	public MonthlyUserPayWizard(MonthlyUserPayModel model) {
@@ -16,13 +21,29 @@ public class MonthlyUserPayWizard extends Wizard implements AbstractWizard{
 
 	@Override
 	public void addPages() {
-		addPage(new MonthlyUserPayBasicPage(model));
+		page = new MonthlyUserPayBasicPage(model);
+		addPage(page);
         setWindowTitle("固定月租用户缴费");
         getShell().setImage(JFaceUtil.getImage("carpark_32"));
 	}
 
 	@Override
 	public boolean performFinish() {
+		if (!model.isFree()) {
+			SingleCarparkMonthlyCharge selectMonth = model.getSelectMonth();
+			if (StrUtil.isEmpty(selectMonth)) {
+				page.setErrorMessage("请选择月租");
+				return false;
+			}
+			if (StrUtil.isEmpty(model.getOverdueTime())||model.getOverdueTime().before(new Date())) {
+				page.setErrorMessage("过期时间必须在现在时间之后");
+				return false;
+			}
+			if (StrUtil.isEmpty(model.getChargesMoney())) {
+				page.setErrorMessage("请输入收费金额");
+				return false;
+			}
+		}
 		return true;
 	}
 
@@ -30,5 +51,4 @@ public class MonthlyUserPayWizard extends Wizard implements AbstractWizard{
 	public MonthlyUserPayModel getModel() {
 		return model;
 	}
-
 }
