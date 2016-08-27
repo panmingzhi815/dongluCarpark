12d41668d4050db0cbaf09035574e51badff4d73
panmingzhi815 <154341736@qq.com>
2016-08-27 17:17:20 +0800

    添加贵州海誉进出记录数据同步测试

--- src/test/java/com/donglu/carpark/service/background/haiyu/AsynHaiYuRecordServiceImplTest.java
+++ src/test/java/com/donglu/carpark/service/background/haiyu/AsynHaiYuRecordServiceImplTest.java
@@ -1,6 +1,12 @@
 package com.donglu.carpark.service.background.haiyu;
 
+import com.dongluhitec.card.domain.db.singlecarpark.haiyu.CarparkRecordHistory;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -8,10 +14,33 @@ import static org.junit.Assert.*;
  * Created by xiaopan on 2016/8/27.
  */
 public class AsynHaiYuRecordServiceImplTest {
+    private final Logger LOGGER = LoggerFactory.getLogger(AsynHaiYuRecordServiceImplTest.class);
+    @Test
+    public void test() throws Exception {
+        HaiYuConfig haiYuConfig = new HaiYuConfig();
+        haiYuConfig.read();
+
+        CarparkRecordHistory carparkRecordHistory = new CarparkRecordHistory();
+        carparkRecordHistory.setId(1L);
+        carparkRecordHistory.setCarparkName("第一车库");
+        carparkRecordHistory.setInTime("2016-08-26 00:12:00");
+        carparkRecordHistory.setInDevice("进口1");
+        carparkRecordHistory.setPlateNO("鄂A74125");
+        carparkRecordHistory.setUserName("小潘");
+
+        List<CarparkRecordHistory> carparkRecordHistories = Arrays.asList(carparkRecordHistory);
+
+        String generateJsonStr = DesUtils.generateJsonStr(haiYuConfig, carparkRecordHistories);
+        LOGGER.info("测试发送数据 {}",generateJsonStr);
+        DesUtils desUtils = new DesUtils(haiYuConfig.getEncryptKey());
+        String post = desUtils.post(haiYuConfig.getCardUsagePostUrl(), generateJsonStr);
+        LOGGER.info("接收测试数据 {}",post);
+    }
 
     @Test
-    public void test(){
-
+    public void create(){
+        HaiYuConfig haiYuConfig = new HaiYuConfig();
+        haiYuConfig.write();
     }
 
 }
